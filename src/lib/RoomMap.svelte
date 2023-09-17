2b577e96a79ef3d7fe81f8138eafb6691e1cd36c
Ayumu Takai <atakai@hackwork.jp>
2023-09-17 16:14:37 +0900

    什器、座席のデータ化

--- src/lib/RoomMap.svelte
+++ src/lib/RoomMap.svelte
@@ -5,6 +5,49 @@
     const activeColor = "red";
     const nonActiveColor = "white";
 
+    const furnitures = [
+        { x: 70, y: 440, width: 350, height: 60, text: "" },
+        { x: 0, y: 354, width: 420, height: 60, text: "" },
+        { x: 0, y: 260, width: 420, height: 60, text: "" },
+        { x: 70, y: 170, width: 350, height: 60, text: "" },
+        { x: 70, y: 80, width: 350, height: 60, text: "" },
+        { x: 70, y: 0, width: 350, height: 60, text: "" },
+
+        { x: 450, y: 440, width: 420, height: 60, text: "" },
+        { x: 450, y: 354, width: 420, height: 60, text: "" },
+        { x: 450, y: 260, width: 420, height: 60, text: "" },
+        { x: 450, y: 170, width: 420, height: 60, text: "" },
+        { x: 450, y: 80, width: 420, height: 60, text: "" },
+        { x: 450, y: 0, width: 420, height: 60, text: "" },
+
+        { x: 900, y: 440, width: 220, height: 60, text: "" },
+        { x: 900, y: 354, width: 220, height: 60, text: "" },
+        { x: 900, y: 260, width: 220, height: 60, text: "" },
+        { x: 900, y: 170, width: 220, height: 60, text: "" },
+        { x: 900, y: 80, width: 220, height: 60, text: "" },
+        { x: 900, y: 0, width: 220, height: 60, text: "" },
+
+        { x: 480, y: 540, width: 220, height: 60, text: "教卓" },
+    ];
+
+    const sheats = [
+        { cx: 110, cy: 384, no: 1 },
+        { cx: 360, cy: 384, no: 2 },
+        { cx: 580, cy: 384, no: 3 },
+        { cx: 820, cy: 384, no: 4 },
+        { cx: 110, cy: 290, no: 5 },
+        { cx: 360, cy: 290, no: 6 },
+        { cx: 580, cy: 290, no: 7 },
+        { cx: 820, cy: 290, no: 8 },
+        { cx: 110, cy: 200, no: 9 },
+        { cx: 360, cy: 200, no: 11 },
+        { cx: 580, cy: 200, no: 12 },
+        { cx: 820, cy: 200, no: 13 },
+        { cx: 580, cy: 110, no: 15 },
+        { cx: 820, cy: 110, no: 14 },
+        { cx: 110, cy: 30, no: 40 },
+    ];
+
     const onClickHndler = (e: Event) => {
         const el = e.target as SVGEllipseElement;
         if (el.style.fill == activeColor) {
@@ -20,50 +63,25 @@
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
         version="1.1"
-        width="1121px"
-        height="601px"
+        width="2000px"
+        height="1500px"
         viewBox="-0.5 -0.5 1121 601"
-        style="background-color: rgb(255, 255, 255);"
-        ><g>
-            <Furniture x={70} y={440} width={350} height={60} />
-            <Furniture x={0} y={354} width={420} height={60} />
-            <Furniture x={0} y={260} width={420} height={60} />
-            <Furniture x={70} y={170} width={350} height={60} />
-            <Furniture x={70} y={80} width={350} height={60} />
-            <Furniture x={70} y={0} width={350} height={60} />
-
-            <Furniture x={450} y={440} width={420} height={60} />
-            <Furniture x={450} y={354} width={420} height={60} />
-            <Furniture x={450} y={260} width={420} height={60} />
-            <Furniture x={450} y={170} width={420} height={60} />
-            <Furniture x={450} y={80} width={420} height={60} />
-            <Furniture x={450} y={0} width={420} height={60} />
-
-            <Furniture x={900} y={440} width={220} height={60} />
-            <Furniture x={900} y={354} width={220} height={60} />
-            <Furniture x={900} y={260} width={220} height={60} />
-            <Furniture x={900} y={170} width={220} height={60} />
-            <Furniture x={900} y={80} width={220} height={60} />
-            <Furniture x={900} y={0} width={220} height={60} />
-
-            <Furniture x={480} y={540} width={220} height={60} text={"教卓"} />
-            <Sheat cx={110} cy={384} no={1} />
-            <Sheat cx={360} cy={384} no={2} />
-            <Sheat cx={580} cy={384} no={3} />
-            <Sheat cx={820} cy={384} no={4} />
-            <Sheat cx={110} cy={290} no={5} />
-            <Sheat cx={360} cy={290} no={6} />
-            <Sheat cx={580} cy={290} no={7} />
-            <Sheat cx={820} cy={290} no={8} />
-            <Sheat cx={110} cy={200} no={9} />
-            <Sheat cx={360} cy={200} no={11} />
-            <Sheat cx={580} cy={200} no={12} />
-            <Sheat cx={820} cy={200} no={13} />
-            <Sheat cx={580} cy={120} no={15} />
-            <Sheat cx={820} cy={120} no={14} />
-
-            <Sheat cx={110} cy={30} no={40} />
-        </g></svg>
+    >
+        <g>
+            {#each furnitures as f}
+                <Furniture
+                    x={f.x}
+                    y={f.y}
+                    width={f.width}
+                    height={f.height}
+                    text={f.text}
+                />
+            {/each}
+            {#each sheats as s}
+                <Sheat cx={s.cx} cy={s.cy} no={s.no} />
+            {/each}
+        </g>
+    </svg>
 </section>
 
 <style>
