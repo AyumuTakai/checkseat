bcfb978eb56076139f831a8574edefb60204cc77
Ayumu Takai <atakai@hackwork.jp>
2023-10-19 19:41:38 +0900

    コードフォーマット

--- src/lib/EventList.svelte
+++ src/lib/EventList.svelte
@@ -3,29 +3,33 @@
 
     // https://ribbit.konomi.app/blog/javascript-date-format/
     const getFormattedDate = (date: Date, format: string) => {
-  const symbol = {
-    M: date.getMonth() + 1,
-    d: date.getDate(),
-    h: date.getHours(),
-    m: date.getMinutes(),
-    s: date.getSeconds(),
-  };
+        const symbol = {
+            M: date.getMonth() + 1,
+            d: date.getDate(),
+            h: date.getHours(),
+            m: date.getMinutes(),
+            s: date.getSeconds(),
+        };
 
-  const formatted = format.replace(/(M+|d+|h+|m+|s+)/g, (v) =>
-    ((v.length > 1 ? "0" : "") + symbol[v.slice(-1) as keyof typeof symbol]).slice(-2)
-  );
+        const formatted = format.replace(/(M+|d+|h+|m+|s+)/g, (v) =>
+            (
+                (v.length > 1 ? "0" : "") +
+                symbol[v.slice(-1) as keyof typeof symbol]
+            ).slice(-2)
+        );
 
-  return formatted.replace(/(y+)/g, (v) =>
-    date.getFullYear().toString().slice(-v.length)
-  );
-};
+        return formatted.replace(/(y+)/g, (v) =>
+            date.getFullYear().toString().slice(-v.length)
+        );
+    };
 </script>
 
 <section>
     <ul>
         {#each $actionLog as event}
             <li>
-                {getFormattedDate(event.datetime,"yyyy/MM/dd hh:mm:ss")} R:{event.room} [{("0" + event.no).slice(-2)}] {event.action}
+                {getFormattedDate(event.datetime, "yyyy/MM/dd hh:mm:ss")} R:{event.room}
+                [{("0" + event.no).slice(-2)}] {event.action}
             </li>
         {/each}
     </ul>
@@ -46,7 +50,7 @@
     li {
         color: black;
         list-style: none;
-        font-family:monospace;
+        font-family: monospace;
     }
     @media (prefers-color-scheme: dark) {
         section {
