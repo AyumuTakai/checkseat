fcb39548f0ccbf0267cfa095319167f6c4347d29
Ayumu Takai <atakai@hackwork.jp>
2023-10-19 19:39:53 +0900

    ログ表示の改善

--- src/lib/EventList.svelte
+++ src/lib/EventList.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
     import { actionLog } from "../actionStore";
+
+    // https://ribbit.konomi.app/blog/javascript-date-format/
+    const getFormattedDate = (date: Date, format: string) => {
+  const symbol = {
+    M: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    m: date.getMinutes(),
+    s: date.getSeconds(),
+  };
+
+  const formatted = format.replace(/(M+|d+|h+|m+|s+)/g, (v) =>
+    ((v.length > 1 ? "0" : "") + symbol[v.slice(-1) as keyof typeof symbol]).slice(-2)
+  );
+
+  return formatted.replace(/(y+)/g, (v) =>
+    date.getFullYear().toString().slice(-v.length)
+  );
+};
 </script>
 
 <section>
     <ul>
         {#each $actionLog as event}
             <li>
-                {event.datetime.toLocaleDateString()}
-                {event.datetime.toLocaleTimeString()} R:{event.room} [{event.no}] {event.action}
+                {getFormattedDate(event.datetime,"yyyy/MM/dd hh:mm:ss")} R:{event.room} [{("0" + event.no).slice(-2)}] {event.action}
             </li>
         {/each}
     </ul>
@@ -28,6 +46,7 @@
     li {
         color: black;
         list-style: none;
+        font-family:monospace;
     }
     @media (prefers-color-scheme: dark) {
         section {
