b613830e11894e1365e1b4b9a31588d99dbeff57
Ayumu Takai <atakai@hackwork.jp>
2023-10-24 21:39:34 +0900

    ルームidを追加

--- src/lib/EventList.svelte
+++ src/lib/EventList.svelte
@@ -1,63 +1,65 @@
 <script lang="ts">
-    import { actionLog } from "../actionStore";
+  import { actionLog } from "../actionStore";
+  import { currentRoom } from "../roomStore";
 
-    // https://ribbit.konomi.app/blog/javascript-date-format/
-    const getFormattedDate = (date: Date, format: string) => {
-        const symbol = {
-            M: date.getMonth() + 1,
-            d: date.getDate(),
-            h: date.getHours(),
-            m: date.getMinutes(),
-            s: date.getSeconds(),
-        };
-
-        const formatted = format.replace(/(M+|d+|h+|m+|s+)/g, (v) =>
-            (
-                (v.length > 1 ? "0" : "") +
-                symbol[v.slice(-1) as keyof typeof symbol]
-            ).slice(-2)
-        );
-
-        return formatted.replace(/(y+)/g, (v) =>
-            date.getFullYear().toString().slice(-v.length)
-        );
+  // https://ribbit.konomi.app/blog/javascript-date-format/
+  const getFormattedDate = (date: Date, format: string) => {
+    const symbol = {
+      M: date.getMonth() + 1,
+      d: date.getDate(),
+      h: date.getHours(),
+      m: date.getMinutes(),
+      s: date.getSeconds(),
     };
+
+    const formatted = format.replace(/(M+|d+|h+|m+|s+)/g, (v) =>
+      (
+        (v.length > 1 ? "0" : "") + symbol[v.slice(-1) as keyof typeof symbol]
+      ).slice(-2)
+    );
+
+    return formatted.replace(/(y+)/g, (v) =>
+      date.getFullYear().toString().slice(-v.length)
+    );
+  };
 </script>
 
 <section>
-    <ul>
-        {#each $actionLog as event}
-            <li>
-                {getFormattedDate(event.datetime, "yyyy/MM/dd hh:mm:ss")} R:{event.room}
-                [{("0" + event.no).slice(-2)}] {event.action}
-            </li>
-        {/each}
-    </ul>
+  <ul>
+    {#each $actionLog.filter((action) => {
+      action.roomId = $currentRoom.id;
+    }) as event}
+      <li>
+        {getFormattedDate(event.datetime, "yyyy/MM/dd hh:mm:ss")} R:{event.room}
+        [{("0" + event.no).slice(-2)}] {event.action}
+      </li>
+    {/each}
+  </ul>
 </section>
 
 <style>
+  section {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    min-height: 50vh;
+    background-color: white;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    color: black;
+    list-style: none;
+    font-family: monospace;
+  }
+  @media (prefers-color-scheme: dark) {
     section {
-        box-sizing: border-box;
-        width: 100%;
-        height: 100%;
-        min-height: 50vh;
-        background-color: white;
-    }
-    ul {
-        margin: 0;
-        padding: 0;
+      background-color: black;
     }
     li {
-        color: black;
-        list-style: none;
-        font-family: monospace;
+      color: white;
     }
-    @media (prefers-color-scheme: dark) {
-        section {
-            background-color: black;
-        }
-        li {
-            color: white;
-        }
-    }
+  }
 </style>
--- src/lib/Seat.svelte
+++ src/lib/Seat.svelte
@@ -1,107 +1,107 @@
 <script lang="ts">
-    import { currentRoom, type Room } from "../roomStore";
-    import { addLog } from "../actionStore";
-    import { attends,Attendees } from "../attendStore";
+  import { currentRoom, type Room } from "../roomStore";
+  import { addLog } from "../actionStore";
+  import { attends, Attendees } from "../attendStore";
 
-    export let no: number = 0;
-    export let cx: number = 0;
-    export let cy: number = 0;
+  export let no: number = 0;
+  export let cx: number = 0;
+  export let cy: number = 0;
 
-    let el: SVGEllipseElement;
+  let el: SVGEllipseElement;
 
-    const writeLog = (msg: string) => {
-        const now = new Date();
-        // 操作ログに追加
-        addLog({
-            room: $currentRoom.name,
-            datetime: now,
-            no,
-            action: msg,
-        });
-        // 出席記録を更新
-        attends.update((att:Attendees)=>{
-            if(msg == 'active') {
-                att.get(no).inactive(now);
-            }else{
-                att.get(no).active(now);
-            }
-            return att;
-        });
-        currentRoom.update((room: Room) => {
-            // if (!attend) {
-            //     attend = {
-            //         no,
-            //         begin: now.getHours() * 100 + now.getMinutes(),
-            //         end: 2400,
-            //     };
-            //     room.attends.push(attend);
-            // } else {
-            //     attend.end = now.getHours() * 100 + now.getMinutes();
-            //     attend = undefined;
-            // }
-            return room;
-        });
-    };
+  const writeLog = (msg: string) => {
+    const now = new Date();
+    // 操作ログに追加
+    addLog({
+      roomId: $currentRoom.id,
+      datetime: now,
+      no,
+      action: msg,
+    });
+    // 出席記録を更新
+    attends.update((att: Attendees) => {
+      if (msg == "active") {
+        att.get(no).inactive(now);
+      } else {
+        att.get(no).active(now);
+      }
+      return att;
+    });
+    currentRoom.update((room: Room) => {
+      // if (!attend) {
+      //     attend = {
+      //         no,
+      //         begin: now.getHours() * 100 + now.getMinutes(),
+      //         end: 2400,
+      //     };
+      //     room.attends.push(attend);
+      // } else {
+      //     attend.end = now.getHours() * 100 + now.getMinutes();
+      //     attend = undefined;
+      // }
+      return room;
+    });
+  };
 
-    const onClickHandler = (e: Event) => {
-        // const el = e.target as SVGEllipseElement;
-        if (el.style.fill == "var(--activeColor)") {
-            el.style.fill = "var(--nonActiveColor)";
-            writeLog("non active");
-        } else {
-            el.style.fill = "var(--activeColor)";
-            writeLog("active");
-        }
-    };
+  const onClickHandler = (e: Event) => {
+    // const el = e.target as SVGEllipseElement;
+    if (el.style.fill == "var(--activeColor)") {
+      el.style.fill = "var(--nonActiveColor)";
+      writeLog("non active");
+    } else {
+      el.style.fill = "var(--activeColor)";
+      writeLog("active");
+    }
+  };
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <ellipse
-    {cx}
-    {cy}
-    rx="20"
-    ry="20"
-    pointer-events="all"
-    on:click={onClickHandler}
-    bind:this={el}
+  {cx}
+  {cy}
+  rx="20"
+  ry="20"
+  pointer-events="all"
+  on:click={onClickHandler}
+  bind:this={el}
 />
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <text
-    x={cx}
-    y={cy}
-    on:click={onClickHandler}
-    text-anchor="middle"
-    dominant-baseline="central">{no}</text
+  x={cx}
+  y={cy}
+  on:click={onClickHandler}
+  text-anchor="middle"
+  dominant-baseline="central">{no}</text
 >
 
 <style>
+  ellipse {
+    --activeColor: lightgreen;
+    --nonActiveColor: white;
+    fill: white;
+    stroke: black;
+  }
+  text {
+    stroke: gray;
+  }
+  text,
+  ellipse {
+    cursor: pointer;
+  }
+  @media (prefers-color-scheme: dark) {
     ellipse {
-        --activeColor: lightgreen;
-        --nonActiveColor: white;
-        fill: white;
-        stroke: black;
+      --activeColor: orange;
+      --nonActiveColor: black;
+      fill: black;
+      stroke: white;
     }
     text {
-        stroke: gray;
+      stroke-width: 3;
+      stroke: black;
+      fill: white;
+      paint-order: stroke;
+      font-weight: bold;
+      stroke-linejoin: round;
     }
-    text,
-    ellipse {
-        cursor: pointer;
-    }
-    @media (prefers-color-scheme: dark) {
-        ellipse {
-            --activeColor: orange;
-            --nonActiveColor: black;
-            fill: black;
-            stroke: white;
-        }
-        text {
-            stroke-width: 3;
-            stroke: black;
-            fill: white;
-            paint-order: stroke;
-            font-weight: bold;
-            stroke-linejoin: round;
-        }
-    }
+  }
 </style>
