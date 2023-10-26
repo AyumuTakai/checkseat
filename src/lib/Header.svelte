a83d4e000bafb2a66bb244c0eefe421324102ac0
Ayumu Takai <atakai@hackwork.jp>
2023-10-27 02:42:13 +0900

    モーダルダイアログを分割

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,53 +1,52 @@
 <script lang="ts">
   import { rooms, currentRoom, createNewRoom } from "../roomStore";
+  import Modal from "./Modal.svelte";
 
-  let dialog: HTMLDialogElement;
+  let showModal: boolean = false;
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<header
-  on:click={() => {
-    dialog.showModal();
-  }}
->
+<header>
+  <h1>
+    <button
+      on:click={() => {
+        showModal = true;
+      }}
+    >
+      {#if $currentRoom}
+        {$currentRoom.name}
+      {:else}
+        Check Seat
+      {/if}
+    </button>
+  </h1>
   {#if $currentRoom}
-    <h1>{$currentRoom.name}</h1>
-  {:else}
-    <h1>Check Seat</h1>
+    <button>Edit</button>
   {/if}
 </header>
-<dialog bind:this={dialog}>
-  <header>
-    <h2>Rooms</h2>
-  </header>
-  <main>
-    <button
-      on:click={() => {
-        const room = createNewRoom();
-        currentRoom.set(room);
-      }}>Create new room</button
-    >
-    {#if $rooms}
-      {#each $rooms as room}
-        <button
-          on:click={() => {
-            currentRoom.set(room);
-            dialog.close();
-          }}>{room.name}</button
-        >
-      {/each}
-    {/if}
-  </main>
-  <footer>
-    <button
-      on:click={() => {
-        dialog.close();
-      }}>Exit</button
-    >
-  </footer>
-</dialog>
+<Modal title="rooms" show={showModal}>
+  <button
+    on:click={() => {
+      const room = createNewRoom();
+      currentRoom.set(room);
+    }}>Create new room</button
+  >
+  {#if $rooms}
+    {#each $rooms as room}
+      <button
+        on:click={() => {
+          currentRoom.set(room);
+          showModal = false;
+        }}>{room.name}</button
+      >
+    {/each}
+  {/if}
+</Modal>
 
 <style>
+  header {
+    display: flex;
+  }
   h1 {
     display: block;
     width: 100%;
@@ -57,33 +56,11 @@
     font-size: 1.4rem;
     height: 2rem;
   }
-  dialog {
-    width: 400px;
-    height: 300px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    margin-left: -200px;
-    margin-top: -150px;
-    border: solid 1px black;
-  }
-  dialog header {
-    width: 100%;
-    background-color: gray;
-    text-align: center;
-  }
-  dialog main {
-    display: flex;
-    flex-direction: column;
-  }
-  dialog footer {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-    left: 0;
-    padding: 0.2rem 1rem;
-  }
-  dialog footer button {
-    width: 100%;
+  h1 button {
+    height: 2rem;
+    background-color: lightblue;
+    color: black;
+    border: solid navy;
+    padding: 0 0.5rem;
   }
 </style>
---
+++ src/lib/Modal.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  export let title: string = "";
+  export let show: boolean = false;
+  let dialog: HTMLDialogElement;
+
+  $: if (dialog) {
+    if (show) {
+      dialog.showModal();
+    } else {
+      dialog.close();
+    }
+  }
+</script>
+
+<dialog bind:this={dialog}>
+  <header>
+    <h2>{title}</h2>
+  </header>
+  <section>
+    <slot />
+  </section>
+  <footer>
+    <button
+      on:click={() => {
+        dialog.close();
+      }}>Exit</button
+    >
+  </footer>
+</dialog>
+
+<style>
+  dialog {
+    min-width: 400px;
+    min-height: 300px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -200px;
+    margin-top: -150px;
+    border: solid 1px black;
+  }
+  dialog header {
+    width: 100%;
+    background-color: gray;
+    text-align: center;
+  }
+  dialog section {
+    display: flex;
+    flex-direction: column;
+  }
+  dialog footer {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    padding: 0.2rem 1rem;
+  }
+  dialog footer button {
+    width: 100%;
+  }
+</style>
