cb1c347fa1999ec98892b30c4d2fbc9001316a99
Ayumu Takai <atakai@hackwork.jp>
2023-11-03 23:10:26 +0900

    fix: 編集画面とチェック画面を切り替えると出席表示がリセットされる #12

--- src/lib/RoomMap.svelte
+++ src/lib/RoomMap.svelte
@@ -1,61 +1,61 @@
 <script lang="ts">
-    import { currentRoom } from "../roomStore";
-    import Furniture from "./Furniture.svelte";
-    import Seat from "./Seat.svelte";
+  import { currentRoom } from "../roomStore";
+  import Furniture from "./Furniture.svelte";
+  import Seat from "./Seat.svelte";
 
-    export let _class:string;
+  export let _class: string;
 
-    const activeColor = "red";
-    const nonActiveColor = "white";
+  const activeColor = "red";
+  const nonActiveColor = "white";
 </script>
 
 <section class={_class}>
-    {#if $currentRoom}
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            version="1.1"
-            width="1200px"
-            height="600px"
-            viewBox="0 0 1150 600"
-        >
-            <g>
-                {#each $currentRoom.furnitures as f}
-                    {f}
-                    <Furniture
-                        x={f.x}
-                        y={f.y}
-                        width={f.width}
-                        height={f.height}
-                        text={f.text}
-                    />
-                {/each}
-                {#each $currentRoom.seats as s}
-                    <Seat cx={s.cx} cy={s.cy} no={s.no} />
-                {/each}
-            </g>
-        </svg>
-    {:else}
-        <p>No Data</p>
-    {/if}
+  {#if $currentRoom}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      width="1200px"
+      height="600px"
+      viewBox="0 0 1150 600"
+    >
+      <g>
+        {#each $currentRoom.furnitures as f}
+          {f}
+          <Furniture
+            x={f.x}
+            y={f.y}
+            width={f.width}
+            height={f.height}
+            text={f.text}
+          />
+        {/each}
+        {#each $currentRoom.seats as s}
+          <Seat cx={s.cx} cy={s.cy} no={s.no} />
+        {/each}
+      </g>
+    </svg>
+  {:else}
+    <p>No Data</p>
+  {/if}
 </section>
 
 <style>
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: white;
+  }
+  @media (prefers-color-scheme: dark) {
+    :global(body) {
+      background-color: black;
+    }
+    section {
+      background-color: black;
+    }
     svg {
-        width: 100%;
-        height: 100%;
-        fill: white;
+      fill: black;
+      stroke: white;
     }
-    @media(prefers-color-scheme: dark) {
-        :global(body) {
-            background-color: black;
-        }
-        section {
-            background-color: black;
-        }
-        svg {
-            fill: black;
-            stroke: white;
-        }
-    }
+  }
 </style>
